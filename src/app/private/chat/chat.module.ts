5cf916fc48a34d1d4d3a35e3f2aaf3599c354047
Gabriel Sánchez <gabionezero@gmail.com>
2023-02-21 16:58:09 +0100

    Add first contacts list version

--- src/app/private/chat/chat.module.ts
+++ src/app/private/chat/chat.module.ts
@@ -5,22 +5,18 @@ import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './pages/chat/chat.component';
 import { MailboxComponent } from './components/mailbox/mailbox.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
-
+import { MatListModule } from '@angular/material/list';
+import { ContactComponent } from './components/contact/contact.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     ChatComponent,
     MailboxComponent,
-    ContactsComponent
+    ContactsComponent,
+    ContactComponent,
   ],
-  imports: [
-    CommonModule,
-    ChatRoutingModule
-  ],
-  exports: [
-    ChatComponent,
-    MailboxComponent,
-    ContactsComponent
-  ]
+  imports: [CommonModule, ChatRoutingModule, MatListModule, MatCardModule],
+  exports: [ChatComponent, MailboxComponent, ContactsComponent],
 })
-export class ChatModule { }
+export class ChatModule {}
