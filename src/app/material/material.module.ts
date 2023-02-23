d30dc2434ae0ad256bc60142d4334c34dc70d015
carmencami <105524744+carmencami@users.noreply.github.com>
2023-02-23 11:40:38 +0100

    falta estilo modal

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { MaterialRoutingModule } from './material-routing.module';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
 
-
-
+const modules = [
+  MatButtonModule,
+  MatFormFieldModule, 
+  ReactiveFormsModule, 
+  MatInputModule, 
+  FormsModule, 
+  MatCardModule,
+  MatDialogModule
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MaterialRoutingModule,
-    MatInputModule,
-    MatFormFieldModule
-  ],
-  exports : [MatButtonModule,
-  MatFormFieldModule, ReactiveFormsModule, MatInputModule, FormsModule]
+  imports: [...modules],
+  exports : [...modules]
 })
 export class MaterialModule { }
