95b7ee8859bb76addd4a7c137e0a43910df0ca5d
Gabriel Sánchez <gabionezero@gmail.com>
2023-03-03 14:57:27 +0100

    Add styles and validation form

--- src/app/private/add-copy/pages/add-copy/add-copy.component.ts
+++ src/app/private/add-copy/pages/add-copy/add-copy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-copy',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-copy.component.scss']
 })
 export class AddCopyComponent implements OnInit {
+  
+  addCopyForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.addCopyForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      condition: new FormControl('', [Validators.required]),
+      photos: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      price: new FormControl('', [Validators.required]),
+      });
+  }
+
+  public formError = (controlName: string, errorName: string) =>{
+    return this.addCopyForm.controls[controlName].hasError(errorName);
+    }
+
+  onSubmit(): void {
+    // display some fireworks
   }
 
 }
