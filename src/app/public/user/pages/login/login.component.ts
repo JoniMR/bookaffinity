6fe7cb06cd22eeee695b9767642ef849a1949123
Tomy <tomasgoizuetadp@gmail.com>
2023-03-31 18:31:33 +0200

    servicio login en proceso

--- src/app/public/user/pages/login/login.component.ts
+++ src/app/public/user/pages/login/login.component.ts
@@ -1,26 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { RegisterComponent } from '../register/register.component';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  email: FormControl;
+  password: FormControl;
 
-email: FormControl;
-password: FormControl;
-
-
-  constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<LoginComponent>) {
+  constructor(
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<LoginComponent>,
+    private loginService: LoginService
+  ) {
     this.email = new FormControl('', [Validators.required, Validators.email]);
     this.password = new FormControl('', [Validators.required]);
   }
 
-  ngOnInit(): void {
+  login() {
+    console.log(this.email.value + ", " + this.password.value)
+    this.loginService.getUserbyEmailAndPassword(this.email.value, this.password.value).subscribe(res => {
+      if(res.id_user){
+        // Guardar usuario en el session storage??? y cerrar modal
+      }
+    })
   }
+
+  ngOnInit(): void {}
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
@@ -37,7 +48,7 @@ password: FormControl;
     return this.password.hasError('password') ? 'Not a valid password' : '';
   }
   openRegister() {
-    this.dialog.open(RegisterComponent)
-    this.dialogRef.close()
+    this.dialog.open(RegisterComponent);
+    this.dialogRef.close();
   }
 }
