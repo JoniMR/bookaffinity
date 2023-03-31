e92a8f263675f646f5ec02c0be4fb180135e6901
JoniMR <jonathanmartinrodriguez@gmail.com>
2023-03-31 15:09:41 +0200

    cloudinary

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,28 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AccountComponent } from './components/account/account.component';
 import { MaterialModule } from '../material/material.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ImgUploadComponent } from './components/img-upload/img-upload.component';
+import { NgxDropzoneModule } from 'ngx-dropzone';
 
 @NgModule({
   declarations: [
     NavbarComponent,
     FooterComponent,
     AccountComponent,
+    ImgUploadComponent,
   ],
   imports: [
     CommonModule,
     SharedRoutingModule,
     MaterialModule,
+    BrowserModule,
+    HttpClientModule,
+    NgxDropzoneModule
   ],
   exports: [
     AccountComponent,
     NavbarComponent,
     FooterComponent,
+    ImgUploadComponent
   ]
 })
 export class SharedModule { }
