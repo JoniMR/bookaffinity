ef58c5f3fcc14f4687fb54e4d5f29d61b0c1cd1a
Gabriel Sánchez <gabionezero@gmail.com>
2023-04-03 12:10:37 +0200

    Add modalÇReview & account services

---
+++ src/app/public/details/models/book.model.ts
@@ -0,0 +1,9 @@
+export interface BookInterface {
+    id_book: number,
+    name: string,
+    summary: string,
+    isbn: string,
+    id_author: number,
+    language: string,
+    status: string
+}
--- src/app/shared/components/account/account.component.ts
+++ src/app/shared/components/account/account.component.ts
@@ -5,12 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
   constructor() {}
     name:string = "Michael";
     imageProfile:string ="https://picsum.photos/300/300";
     userName:string = "Michael123";
     bio:string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and";
     location:string = "Málaga, Spain";
+
+    ngOnInit(): void{
+      //getUSerById(){
+
+      //}
+    }
 
 }
